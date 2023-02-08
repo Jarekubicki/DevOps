21acfa7f8750171ee0710a45e4bc5fd0ba120cea
Jarek Kubicki <jkubicki@o2.pl>
2023-02-08 22:03:56 +0100

    files updated

--- src/tematyka8/excercises/Task8_6/Person.java
+++ src/tematyka8/excercises/Task8_6/Person.java
@@ -3,16 +3,24 @@ package tematyka8.excercises.Task8_6;
 /**
  * Created by BIURO22 on 2023-02-06
  */
-public abstract class Person {
+public class Person {
 
     String name;
     int phoneNumber;
     String email;
+    Address address;
 
     public Person(String name, int phoneNumber, String email) {
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.email = email;
+    }
+
+    public Person(String name, int phoneNumber, String email, Address address) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
     }
 
     public void purchaseParkingPass() {
