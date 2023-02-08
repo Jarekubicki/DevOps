fa0b3c6f93ce442d0030a8081cd3abb9d3b768e6
Jarek Kubicki <jkubicki@o2.pl>
2023-02-08 22:54:04 +0100

    Task9_4 file Test.java updated

--- src/tematyka9/excercises/Task9_4/Test.java
+++ src/tematyka9/excercises/Task9_4/Test.java
@@ -6,7 +6,6 @@ package tematyka9.excercises.Task9_4;
 public class Test {
     public static void main(String[] args) {
 
-
         CalculationSystem calculationSystem = new CalculationSystem();
 
         calculationSystem.performCalculations(151, 4);
